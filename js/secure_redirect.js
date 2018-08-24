f524fdd0a5c475c0d16d79a14ec33f5f8d499023
Fernando Flores Garcia <fflores10@uabc.edu.mx>
2018-08-24 11:57:33 -0700

    ajustado la conexión segura

--- js/secure_redirect.js
+++ js/secure_redirect.js
@@ -1,4 +1,19 @@
-if (location.protocol != 'https:')
-{
-location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+    var url = 'https:' + window.location.href.substring(window.location.protocol.length);
+
+    xhr.open('HEAD', url, true);
+    xhr.send();
+     
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(e) {
+      if (xhr.readyState == 4) {
+        if (xhr.status >= 200 && xhr.status < 304) {
+          location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+        }
+      }
+    }
 }
+
+doesConnectionExist()
