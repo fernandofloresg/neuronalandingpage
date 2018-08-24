9bfc4b3ff926187ed6a31263f735dcbe21d0b53f
Fernando Flores Garcia <fflores10@uabc.edu.mx>
2018-08-24 13:16:31 -0700

    resolviendo loop en secure mode

--- js/secure_redirect.js
+++ js/secure_redirect.js
@@ -4,16 +4,22 @@ function doesConnectionExist() {
 
     xhr.open('HEAD', url, true);
     xhr.send();
-     
+
     xhr.addEventListener("readystatechange", processRequest, false);
 
     function processRequest(e) {
       if (xhr.readyState == 4) {
         if (xhr.status >= 200 && xhr.status < 304) {
           location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+        } else {
+          alert("connection doesn't exist!");
         }
       }
     }
 }
 
-doesConnectionExist()
+
+if (location.protocol != 'https:')
+{
+  doesConnectionExist()
+}
